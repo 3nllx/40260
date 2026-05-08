02b372ae24427e52aa5638e000d8f37bda9c7e23
3nl44 <hernandezochoaemanuel22@gmail.com>
2026-05-08 11:25:09 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -4,10 +4,10 @@ let botones = document.getElementsByTagName("button");
 for (const key in botones) {
     if (Object.prototype.hasOwnProperty.call(botones, key)) {
         const boton = botones[key];
-        if (boton.className !="operadores")
-            boton.addEventListener("click", pintar)
+        if (boton.className != "operadores")
+            boton.addEventListener("click", pintar);
         else
-            boton.addEventListener("click", pintar2)
+            boton.addEventListener("click", pintar2);
     }
 }
 
@@ -16,28 +16,49 @@ function pintar(e) {
     resultado.value += e.target.innerText;
 }
 
-let operadores = document.getElementsByClassName("operadores")
-for (const key in operadores){
-    if (Object.prototype.hasOwnProperty.call(operadores, key)){
+let operadores = document.getElementsByClassName("operadores");
+for (const key in operadores) {
+    if (Object.prototype.hasOwnProperty.call(operadores, key)) {
         const boton = operadores[key];
-        boton.addEventListener("click", pintar2)
+        boton.addEventListener("click", pintar2);
     }
 }
 
-function pintar2(e){
-    console.log(e.target.innerText)
-    if(e.target.innerText == "+")
-        suma()
+let prm1, prm2, operacion;
 
+function pintar2(e) {
+    console.log(e.target.innerText);
+    if (e.target.innerText == "+" || e.target.innerText == "-" || e.target.innerText == "*" || e.target.innerText == "/") {
+        prm1 = resultado.value;
+        operacion = e.target.innerText;
+        resultado.value = "";
+    }
+    if (e.target.innerText == "Ce") {
+        resultado.value = "";
+    }
+    if (e.target.innerText == "=") {
+        igual();
+    }
 }
 
-function suma(){
-    prm1 = resultado.value
-    resultado.value = ""
-
-}
-
-function igual(){
-    prm2 = resultado.value
-    resultado.value = parseInt(prm1) + parseInt(prm2)
-}
+function igual() {
+    prm2 = resultado.value;
+    switch (operacion) {
+        case "+":
+            resultado.value = parseFloat(prm1) + parseFloat(prm2);
+            break;
+        case "-":
+            resultado.value = parseFloat(prm1) - parseFloat(prm2);
+            break;
+        case "*":
+            resultado.value = parseFloat(prm1) * parseFloat(prm2);
+            break;
+        case "/":
+            if (prm2 == "0") {
+                resultado.value = "Error";
+            } else {
+                resultado.value = parseFloat(prm1) / parseFloat(prm2);
+            }
+            break;
+    }
+}//este es el javascript 02
